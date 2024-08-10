c420120dd7818ac98182361ba562478886d70581
carlosSimplicio <carlos.simplicio@passeidireto.com>
2024-08-10 00:03:09 -0300

    first simple server

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {}
+
+func main() {
+	PORT := 8080
+	http.HandleFunc("/hello", handleHello)
+
+	fmt.Println("Starting server at port:", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil))
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World - %v", r.Method)	
+}
